1d1edb302984d970dc0d692d63f9174bb5b8ceaa
Carlos Andrés Arévalo <37085951+carevalor@users.noreply.github.com>
2022-04-06 22:05:45 -0500

    Add files via upload

--- myTeleopKey.py
+++ myTeleopKey.py
@@ -1,26 +1,42 @@
-from cgi import print_environ
-from tkinter import W
 import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.srv import TeleportAbsolute, TeleportRelative
 import termios, sys, os
 from numpy import pi
-from pynput.keyboard import Key, Listener, Controller
+from turtlesim.srv import TeleportAbsolute
 
-TERMIOS= termios
-keyboard = Controller()
 
-def on_press(key):
-    if key == Key.space:
-        pubVel(0,1,2.2)
-    #if key == keyboard.press('w'):
-        #pubVel(0,1,2.2)
-    
-def on_release(key):
-    if key == Key.esc:
-        return False
+def pubVel(vel_x, ang_z, t): #Publicador de velocidad en el tópico cmd_ve
+    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    rospy.init_node('velPub', anonymous=False)
+    vel = Twist()
+    vel.linear.x = vel_x
+    vel.angular.z = ang_z
+    #rospy.loginfo(vel)
+    endTime = rospy.Time.now() + rospy.Duration(t)
+    while rospy.Time.now() < endTime:
+        pub.publish(vel)
 
-def getkey():
+def teleport(x, y, ang): #Teletransporte para volver a posición de InicioT
+    rospy.wait_for_service('/turtle1/teleport_absolute')
+    try:
+        teleportA = rospy.ServiceProxy('/turtle1/teleport_absolute', TeleportAbsolute)
+        resp1 = teleportA(x, y, ang)
+        print('Teleported to x: {}, y: {}, ang: {}'.format(str(x),str(y),str(ang)))
+    except rospy.ServiceException as e:
+        print(str(e))
+
+def rel_tp(x, ang): #Función de Teletranspe Relativo para Giro de 180°
+    rospy.wait_for_service('/turtle1/teleport_relative')
+    try:
+        teleportR = rospy.ServiceProxy('/turtle1/teleport_relative', TeleportRelative)
+        resp1 = teleportR(x,ang)
+        print('Teleported to x: {}, ang: {}'.format(str(x),str(ang)))
+    except rospy.ServiceException as e:
+        print(str(e))
+
+TERMIOS= termios 
+def getkey(): #Detectar la Letra
     fd = sys.stdin.fileno()
     old = termios.tcgetattr(fd)
     new = termios.tcgetattr(fd)
@@ -34,31 +50,30 @@ def getkey():
     finally:
         termios.tcsetattr(fd, TERMIOS.TCSAFLUSH, old)
     return c
-
-
-def pubVel(vel_x, ang_z, t):
-    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-    rospy.init_node('velPub', anonymous=False)
-    vel = Twist()
-    vel.linear.x = vel_x
-    vel.angular.z = ang_z
-    #rospy.loginfo(vel)
-    endTime = rospy.Time.now() + rospy.Duration(t)
-    while rospy.Time.now() < endTime:
-        pub.publish(vel)
-
-def check(tecla):
-    if (tecla==b'w'):
+    
+def check(key): #Revisar que tecla fue pesionada
+    if (key==b'w'):
         pubVel(1,0,1)
-    if (tecla==b's'):
+    if (key==b's'):
         pubVel(-1,0,1)
-        
+    if (key==b'a'):
+        pubVel(0,1,1)
+    if (key==b'd'):
+        pubVel(0,-1,1)
+    if (key==b' '):
+        rel_tp(0, pi)
+    if (key==b'r'):
+        teleport(5, 5, 0)
+    if (key==b'q'):
+        exit()
+
 
 if __name__ == '__main__':
     while(1):
-        pubVel(0,0,0.1)
-        tecla=getkey();
-        print(tecla)
-        check(tecla);
-
+        try:
+            pubVel(0,0,0.1)
+            key=getkey(); # Guarda la tecla presionada
+            check(key); #Revisa que tecla fue presionada para operar
+        except rospy.ROSInterruptException:
+            pass
     
